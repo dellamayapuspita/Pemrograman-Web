f44a7da49c7904730d17fb72558d6d1e84e74c0e
dellamayapuspita <dellamayap@gmail.com>
2020-10-14 18:40:01 +0700

    third commit

---
+++ cv.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>CURRICULUM VITAE</title>
+
+	<style type="text/css">
+	body { 
+	background-color: white;
+	}
+	</style>
+
+	<link rel="stylesheet" type="text/css" href="960.css">
+
+</head>
+
+<body>
+
+<center>
+
+	<div class="container_12" style="border: 1px solid #000;">
+		<div class="grid_12" style="background: gray;">
+		<img src="del.jpg" height="250" width="250">
+		</div>
+	</div>
+	
+	<div class="container_12" style="border: 1px solid #000;">
+		<div class="grid_12" style="background: gray;">
+		
+		<?php
+		$Name= "DELLA MAYA PUSPITA";
+		$Address= "Pojok RT/RW 006/002 Pandansari, Ngunut, Tulungagung (66292)";
+		$Phone= "085755821805";
+		$Email= "dellamayap@gmail.com";
+
+		echo "<h2>$Name</h2>";
+		echo "<h4>$Address</h4>";
+		echo "<h4>$Phone * $Email</h4>";
+		
+		?>
+		
+		</div>
+	</div>
+	
+</center>
+	
+	<div class="container_12" style="border: 1px solid #000;">
+		<div class="grid_12" style="background: gray;">
+		
+<h4><u>Pendidikan:</u></h4>
+<table>
+        <tr>
+            <td><b>Universitas Pembangunan Nasional “Veteran” Jawa Timur<b></td>
+            <td>Surabaya</td>
+        </tr>
+        <tr>
+            <td>Program Studi Informatika, Fakultas Ilmu Komputer</td>
+            <td>2018-now</td>
+        </tr>
+		<tr>
+            <td>•Sekretaris 1 Himpunan Mahasiswa Teknik Informatika</td>
+            <td>2020</td>
+        </tr>
+		<tr>
+            <td>•Kewirausahaan Forum Mahasiswa Tulungagung Se-Surabaya</td>
+            <td>2019</td>
+        </tr>
+		<tr>
+            <td>•Advokesma Unit Kegiatan Mahasiswa Renang</td>
+            <td>2018</td>
+        </tr>
+		<tr>
+            <td>•LKMM-TD Fakultas Ilmu Komputer</td>
+            <td>2018</td>
+        </tr>
+		<tr>
+            <td><b>SMAN 1 Kedungwaru, Tulungagung<b></td>
+        </tr>
+		<tr>
+            <td>Jurusan MIPA</td>
+            <td>2015-2018</td>
+        </tr>
+		<tr>
+            <td>•Humas Pasukan Pengibar Bendera SMAN 1 Kedungwaru</td>
+            <td>2016</td>
+        </tr>
+		<tr>
+            <td>•Patroli Keamanan Sekolah SMAN 1 Kedungwaru</td>
+            <td>2016</td>
+        </tr>
+		<tr>
+            <td>•Kelompok Ilmiah Remaja SMAN 1 Kedungwaru</td>
+            <td>2016</td>
+        </tr>
+		<tr>
+            <td><b>SMPN 1 Ngunut, Tulungagung<b></td>
+            <td>2012-2015</td>
+        </tr>
+		<tr>
+            <td><b>SDN 1 Pandansari, Tulungagung<b></td>
+            <td>2006-2012</td>
+        </tr>
+</table>
+
+		</div>
+	</div>
+	
+	<div class="container_12" style="border: 1px solid #000;">
+		<div class="grid_12" style="background: gray;">
+
+<h4><u>Keahlian:</u></h4>
+
+		<?php
+		$O= "Organisasi";
+		$K= "Kemampuan Berbicara";
+		$MW= "Ms. Word";
+		$MP= "Ms. Power Point";
+		$ME= "Ms. Excel";
+		$A= "Adobe Photosop";
+		$H= "HTML";
+		$C= "CSS";
+
+		echo "$O		:	*****</br>";
+		echo "$K		:	****</br>";
+		echo "$MW		:	****</br>";
+		echo "$MP		:	****</br>";
+		echo "$ME		:	***</br>";
+		echo "$A		:	***</br>";
+		echo "$H		:	***</br>";
+		echo "$C		:	***";
+		
+		?>
+		
+		</div>	 
+	</div>
+
+</body>
+</html>
